5f2f534bdbcb50f613f95187961315e798650f64
Matt Bostock <matt@mattbostock.com>
2015-06-09 17:17:17 +0200

    Move common test setup to separate function
    Move common test setup to a separate function so that it can be reused.

--- ical_test.go
+++ ical_test.go
@@ -7,17 +7,28 @@ import (
 	"time"
 )
 
-func testEncode(t *testing.T) {
+func testSetup(vComponents []VComponent) (bytes.Buffer, error) {
 	c := NewBasicVCalendar()
 	c.PRODID = "proid"
 	c.X_WR_TIMEZONE = "Asia/Tokyo"
 	c.X_WR_CALNAME = "name"
 	c.X_WR_CALDESC = "desc"
 
+	c.VComponent = vComponents
+
+	var b bytes.Buffer
+	if err := c.Encode(&b); err != nil {
+		return b, err
+	}
+
+	return b, nil
+}
+
+func testEncode(t *testing.T) {
 	zone := time.FixedZone("Asia/Tokyo", 60*60*9)
 	d := time.Date(2014, time.Month(1), 1, 0, 0, 0, 0, zone)
 
-	c.VComponent = []VComponent{
+	vComponents := []VComponent{
 		&VEvent{
 			UID:     "123",
 			DTSTAMP: d,
@@ -28,8 +39,8 @@ func testEncode(t *testing.T) {
 		},
 	}
 
-	var b bytes.Buffer
-	if err := c.Encode(&b); err != nil {
+	b, err := testSetup(vComponents)
+	if err != nil {
 		t.Error("got err:", err)
 	}
 
