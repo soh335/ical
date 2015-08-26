bbe10fff4dd449faecd23803070dae461e7d8962
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2015-08-26 14:01:15 +0100

    add draft properties

--- ical.go
+++ ical.go
@@ -14,14 +14,46 @@ const (
 	dateTimeLayout = "20060102T150405"
 )
 
-type VCalendar struct {
-	PRODID   string
-	VERSION  string
-	CALSCALE string
+/*
+  http://tools.ietf.org/html/draft-daboo-icalendar-extensions-09
+  http://stackoverflow.com/a/17187346/195141
 
-	X_WR_CALNAME  string
-	X_WR_TIMEZONE string
-	X_WR_CALDESC  string
+  BEGIN:VCALENDAR
+  VERSION:2.0
+  PRODID:-//My Company//NONSGML Event Calendar//EN
+  URL:http://my.calendar/url
+  NAME:My Calendar Name
+  X-WR-CALNAME:My Calendar Name
+  DESCRIPTION:A description of my calendar
+  X-WR-CALDESC:A description of my calendar
+  TIMEZONE-ID:Europe/London
+  X-WR-TIMEZONE:Europe/London
+  REFRESH-INTERVAL;VALUE=DURATION:PT12H
+  X-PUBLISHED-TTL:PT12H
+  COLOR:34:50:105
+  CALSCALE:GREGORIAN
+  METHOD:PUBLISH
+*/
+
+type VCalendar struct {
+	VERSION string // 2.0
+	PRODID  string // -//My Company//NONSGML Event Calendar//EN
+	URL     string // http://my.calendar/url
+
+	NAME         string // My Calendar Name
+	X_WR_CALNAME string // My Calendar Name
+	DESCRIPTION  string // A description of my calendar
+	X_WR_CALDESC string // A description of my calendar
+
+	TIMEZONE_ID   string // Europe/London
+	X_WR_TIMEZONE string // Europe/London
+
+	REFRESH_INTERVAL string // PT12H
+	X_PUBLISHED_TTL  string // PT12H
+
+	COLOR    string // 34:50:105
+	CALSCALE string // GREGORIAN
+	METHOD   string // PUBLISH
 
 	VComponent []VComponent
 }
@@ -44,10 +76,18 @@ func (c *VCalendar) Encode(w io.Writer) error {
 	attrs := []map[string]string{
 		{"VERSION:": c.VERSION},
 		{"PRODID:": c.PRODID},
+		{"URL:": c.URL},
+		{"NAME:": c.NAME},
 		{"X-WR-CALNAME:": c.X_WR_CALNAME},
+		{"DESCRIPTION:": c.DESCRIPTION},
 		{"X-WR-CALDESC:": c.X_WR_CALDESC},
+		{"TIMEZONE-ID:": c.TIMEZONE_ID},
 		{"X-WR-TIMEZONE:": c.X_WR_TIMEZONE},
+		{"REFRESH-INTERVAL;VALUE=DURATION:": c.REFRESH_INTERVAL},
+		{"X-PUBLISHED-TTL:": c.X_PUBLISHED_TTL},
+		{"COLOR:": c.COLOR},
 		{"CALSCALE:": c.CALSCALE},
+		{"METHOD:": c.METHOD},
 	}
 
 	for _, item := range attrs {
--- ical_test.go
+++ ical_test.go
@@ -97,6 +97,58 @@ END:VCALENDAR
 	}
 }
 
+func TestEncodeDraftProperties(t *testing.T) {
+	zone := time.FixedZone("Asia/Tokyo", 60*60*9)
+	d := time.Date(2014, time.Month(1), 1, 0, 0, 0, 0, zone)
+
+	vComponents := []VComponent{
+		&VEvent{
+			UID:     "123",
+			DTSTAMP: d,
+			DTSTART: d,
+			DTEND:   d,
+			SUMMARY: "summary",
+			TZID:    "Asia/Tokyo",
+		},
+	}
+
+	b, err := testSetupWithDraft(vComponents)
+	if err != nil {
+		t.Error("got err:", err)
+	}
+
+	expect := `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:proid
+URL:http://my.calendar/url
+NAME:name
+X-WR-CALNAME:name
+DESCRIPTION:desc
+X-WR-CALDESC:desc
+TIMEZONE-ID:Asia/Tokyo
+X-WR-TIMEZONE:Asia/Tokyo
+REFRESH-INTERVAL;VALUE=DURATION:PT12H
+X-PUBLISHED-TTL:PT12H
+COLOR:34:50:105
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+BEGIN:VEVENT
+DTSTAMP:20131231T150000Z
+UID:123
+TZID:Asia/Tokyo
+SUMMARY:summary
+DTSTART;TZID=Asia/Tokyo;VALUE=DATE-TIME:20140101T000000
+DTEND;TZID=Asia/Tokyo;VALUE=DATE-TIME:20140101T000000
+END:VEVENT
+END:VCALENDAR
+`
+	expect = unixToDOSLineEndings(expect)
+
+	if s := b.String(); s != expect {
+		t.Errorf("should %v. but got %v", expect, s)
+	}
+}
+
 func unixToDOSLineEndings(input string) string {
 	return strings.Replace(input, "\n", "\r\n", -1)
 }
@@ -107,6 +159,32 @@ func testSetup(vComponents []VComponent) (bytes.Buffer, error) {
 	c.X_WR_TIMEZONE = "Asia/Tokyo"
 	c.X_WR_CALNAME = "name"
 	c.X_WR_CALDESC = "desc"
+
+	c.VComponent = vComponents
+
+	var b bytes.Buffer
+	if err := c.Encode(&b); err != nil {
+		return b, err
+	}
+
+	return b, nil
+}
+
+func testSetupWithDraft(vComponents []VComponent) (bytes.Buffer, error) {
+	c := NewBasicVCalendar()
+	c.PRODID = "proid"
+	c.URL = "http://my.calendar/url"
+	c.NAME = "name"
+	c.X_WR_CALNAME = "name"
+	c.DESCRIPTION = "desc"
+	c.X_WR_CALDESC = "desc"
+	c.TIMEZONE_ID = "Asia/Tokyo"
+	c.X_WR_TIMEZONE = "Asia/Tokyo"
+	c.REFRESH_INTERVAL = "PT12H"
+	c.X_PUBLISHED_TTL = "PT12H"
+	c.COLOR = "34:50:105"
+	c.CALSCALE = "GREGORIAN"
+	c.METHOD = "PUBLISH"
 
 	c.VComponent = vComponents
 
